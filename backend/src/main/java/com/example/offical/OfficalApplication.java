44012175c219d456c269b6f69995def6fae43d03
Nguyenvaan77 <anvan16022005@gmail.com>
2025-08-03 05:33:22 +0700

    init

---
+++ backend/src/main/java/com/example/offical/OfficalApplication.java
@@ -0,0 +1,13 @@
+package com.example.offical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/offical/OfficalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.offical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
